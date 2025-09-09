1f622aeb5e156f198d18de26bd1dc7994fa60347
Nitya Kashyap <119543578+nityakashyap7@users.noreply.github.com>
2025-09-08 20:49:40 -0700

    Update all_ckpts_test.sh
    misspelled hlmeh directory name in cd command

--- all_ckpts_test.sh
+++ all_ckpts_test.sh
@@ -1,6 +1,6 @@
 MODEL="hubble-1b-100b_toks-perturbed-hf"
 mapfile -t LST < <(git ls-remote --refs https://huggingface.co/allegrolab/$MODEL | grep -o 'step[0-9]\+')
-cd hmleh
+cd hlmeh
 # let's just test 3 checkpoints
 count=0
 for REVISION in "${LST[@]}"; do
